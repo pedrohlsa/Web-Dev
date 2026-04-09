02bcfecda13afd2ce80e881025535972a3982aac
pedrohlsa <stguts25@gmail.com>
2026-04-08 21:28:57 -0300

    Create testestyle.php

---
+++ css-basics/testestyle.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+body {
+background-color: linen;
+}
+h1 {
+    color: maroon;
+    margin-left: 40px;
+}
+h2 {
+    color: brown;
+    margin-left: 70px;
+}
+p {
+    text-align: justify;
+} 
+</style>
+</head>
+<body>
+<h1> Título com h1</h1>
+    <p>Texto do parágrafo. Estudo sobre o uso de CSS em paginas web com HTML.</p>
+<h2> Título com h2</h2>
+    <p>Com CSS, é possivel alterar o estilo de paginas. Neste exemplo estamos usando o CSS interno.</p>
+</body>
+</html>
